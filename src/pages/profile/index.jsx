57cbc242c499fd06589358ed0a0bd571dfea73e5
komathi D <komathidurai18@gmail.com>
2025-01-30 20:28:46 +0530

    updated login page and Auth

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useAppStore } from '@/store';
+
 
 const Profile = () => {
+  const userInfo = useAppStore((state) => state.userInfo);
+
   return (
     <div>
       Profile
+      <div>Email:{userInfo.id}</div>
       </div>
-  )
-}
+  );
+};
 
 export default Profile;
