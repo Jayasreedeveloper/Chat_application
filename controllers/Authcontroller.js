dba60deba244a8e7dfea25d9f1bb1757ff5c2e2c
mohan raj <mohancr700@gmail.com>
2025-01-30 14:07:42 +0530

    use authRoutes in index.js

--- controllers/Authcontroller.js
+++ controllers/Authcontroller.js
@@ -1,11 +1,13 @@
-import User from"../models/UserModel";
-import {sign} from "jsonwebtoken"
+import User from"../models/UserModel.js";
+import jwt from "jsonwebtoken";
 const maxAge= 3*24*60*60*1000;
 
 const createToken=(email,userId)=>{
-    return sign({email,userId},process.env.JWT_KEY,){};
+    return jwt.sign({email,userId},process.env.JWT_KEY,{
+        expiresIn:maxAge,
+    });
 
-    }
+    };
 
   
 export const signup = async (request,response,next) =>{
@@ -15,7 +17,20 @@ export const signup = async (request,response,next) =>{
         return response.status(400).send("Email and password is required")
     }
     const user=await User.create({email,password});
-
+    response.cookie("jwt",createToken(email,user.id),{
+        maxAge,
+        secure: true,
+        sameSite:"None",
+    });
+    return response.ststus(201).json({ 
+    user:{
+     id:user.id,
+     email:user.email,
+     firstName:user.firstName,
+     lastName:user.image,
+     profileSetup:user.profileSetup,
+      },
+    });
     }catch(error){
         console.log({error});
         return response.status(500).send("Internal Server Error");
