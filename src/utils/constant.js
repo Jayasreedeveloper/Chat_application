44b5960016f6b387696e1afb4b17814f83898154
Vibinraj-unique <vibin170315@gmail.com>
2025-01-31 15:14:46 +0530

    profile image started

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,3 +4,4 @@ export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = "${AUTH_ROUTES}/signup";
 export const LOGIN_ROUTE = "${AUTH_ROUTE}/Login";
 export const GET_USER_INFO = "${AUTH_ROUTES}/userInfo";
+export const UPDATE_PROFILE_ROUTE = "${AUTH_ROUTES}/updateProfile";
